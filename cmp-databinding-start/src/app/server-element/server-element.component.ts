57ddb095c5bf110512c705e3d93fdd9985d3c7a5
kauanrfontana <kauanrfontana@gmail.com>
2023-06-13 17:44:31 -0300

    add OnChanges

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,25 @@
 import { ViewEncapsulation } from '@angular/compiler';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: {type: string, name:string, content:string};
 
-  constructor() {} 
+  constructor() {
+    console.log("construtor chamado!")
+  } 
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges chamado!")
+    console.log(changes)
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit chamado!")
   }
 
 }
