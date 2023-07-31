1e3c738b469e442e28d30e45c62bc478c2ff5e47
kauanrfontana <kauanrfontana@gmail.com>
2023-07-31 17:30:39 -0300

    updated

---
+++ obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const customIntevalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2 ) observer.complete();
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    
+
+    this.firstObsSubscription = customIntevalObservable.pipe(map((data:number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert(error.message);
+    }, () => {
+      console.log('Completed!')
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
