b6054c263f9cff36f738a8fe105e163c5a713188
kauanrfontana <kauanrfontana@gmail.com>
2024-05-13 17:39:44 -0300

    add animations

---
+++ animations/src/app/app.component.ts
@@ -0,0 +1,166 @@
+import {
+  animate,
+  group,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component } from "@angular/core";
+import { transform } from "cypress/types/lodash";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px)",
+        })
+      ),
+      transition("normal <=> highlighted", animate(300)),
+      // transition('highlighted => normal', animate(800)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          backgroundColor: "red",
+          transform: "translateX(0) scale(1)",
+          borderRadius: "0",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+          borderRadius: "0",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0) scale(0.5)",
+        })
+      ),
+
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({ backgroundColor: "orange" }), // Define o estilo inicial da transição
+        animate(1000, style({ borderRadius: "50px" })), // Animação do borderRadius
+        animate(500), // Animação final
+      ]),
+    ]),
+    trigger("list1", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        })
+      ),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          transform: "translateX(-100px)",
+        }),
+        animate(300),
+      ]),
+      transition("* => void", [
+        group([
+          animate(300, style({
+            color: "red"
+          })),
+          animate(800, style({
+            transform: "translateX(100px)",
+            opacity: 0
+          })),
+        ])
+      ]),
+    ]),
+    trigger("list2", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        })
+      ),
+      transition("void => *", [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ])),
+      ]),
+      transition("* => void", [
+        group([
+          animate(300, style({
+            color: "red"
+          })),
+          animate(800, style({
+            transform: "translateX(100px)",
+            opacity: 0
+          })),
+        ])
+      ]),
+    ]),
+  ],
+})
+export class AppComponent {
+  state = "normal";
+  wildState = "normal";
+  list = ["Milk", "Sugar", "Bread"];
+
+  onAdd(item: string) {
+    this.list.push(item);
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
+  }
+
+  onAnimate() {
+    this.state == "normal"
+      ? (this.state = "highlighted")
+      : (this.state = "normal");
+    this.wildState == "normal"
+      ? (this.wildState = "highlighted")
+      : (this.wildState = "normal");
+  }
+
+  onDelete(item: string) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+}
